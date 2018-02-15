6c5eec902adda122c63d40f2c1c5e1a3cd0a35ce
laszlosebok <hun.slaszlo@gmail.com>
2018-02-15 12:30:11 +0100

    feat(HelloWorld): Hello World in Java

---
+++ week-01/day-3/src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Outputs "Hello World!" to the console window
+        System.out.println("Hello World!");
+    }
+}
