91bad4219df610c0039eaf11ddc5e8812042c33b
laszlosebok <hun.slaszlo@gmail.com>
2018-02-15 14:07:19 +0100

    feat(Variables): Exercise 10

---
+++ week-01/day-3/src/DefineBasicInfo.java
@@ -0,0 +1,17 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String myName = "Laszlo Sebok";
+        System.out.println(myName);
+        // Your age as an integer
+        int myAge = 26;
+        System.out.println(myAge);
+        // Your height in meters as a double
+        double myHeight = 1.83;
+        System.out.println(myHeight);
+        // Whether you are married or not as a boolean
+        boolean isMarried = false;
+        System.out.println(isMarried);
+    }
+}
