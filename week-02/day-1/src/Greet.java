844ad1fdfa75264818d1b666e2063c6cd934b014
laszlosebok <hun.slaszlo@gmail.com>
2018-02-19 12:37:39 +0100

    feat(Functions): Exercise 2

---
+++ week-02/day-1/src/Greet.java
@@ -0,0 +1,16 @@
+public class Greet {
+
+    public static void main(String[] args) {
+        // - Create a string variable named `al` and assign the value `Greenfox` to it
+        // - Create a function called `greet` that greets it's input parameter
+        // - Greeting is printing e.g. `Greetings dear, Greenfox`
+        // - Greet `al`
+
+        String al = "Greenfox";
+        greet(al);
+    }
+
+    public static void greet(String personToGreet) {
+        System.out.println("Greetings dear, " + personToGreet);
+    }
+}
