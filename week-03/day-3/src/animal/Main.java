ebc4f77c49b29e4320b789de3c6fe1661453ba12
laszlosebok <hun.slaszlo@gmail.com>
2018-02-28 12:47:07 +0100

    feat(Encapsulation): Exercise 6

---
+++ week-03/day-3/src/animal/Main.java
@@ -0,0 +1,12 @@
+package animal;
+
+public class Main {
+  public static void main(String[] args) {
+    Animal tiger = new Animal();
+    tiger.play();
+    
+    Animal lion = new Animal();
+    lion.eat();
+    lion.drink();
+  }
+}
