02a9b4b6aed7a570a313739def2c33593c0ed67d
laszlosebok <hun.slaszlo@gmail.com>
2018-02-28 11:31:07 +0100

    feat(Data Structure): Exercise 1

---
+++ week-03/day-3/src/PostIt/Main.java
@@ -0,0 +1,13 @@
+package PostIt;
+
+public class Main {
+  public static void main(String[] args) {
+    PostIt example1 = new PostIt("orange", "Idea 1", "blue");
+    PostIt example2 = new PostIt("pink", "Awesome", "black");
+    PostIt example3 = new PostIt("yellow", "Superb!", "green");
+  
+    System.out.println(example1.toString());
+    System.out.println(example2.toString());
+    System.out.println(example3.toString());
+  }
+}
---
+++ week-03/day-3/src/PostIt/PostIt.java
@@ -0,0 +1,22 @@
+package PostIt;
+
+public class PostIt {
+  private String backgroundColor;
+  private String text;
+  private String textColor;
+  
+  public PostIt(String backgroundColor, String text, String textColor) {
+    this.backgroundColor = backgroundColor;
+    this.text = text;
+    this.textColor = textColor;
+  }
+  
+  @Override
+  public String toString() {
+    return "PostIt{" +
+      "backgroundColor='" + backgroundColor + '\'' +
+      ", text='" + text + '\'' +
+      ", textColor='" + textColor + '\'' +
+      '}';
+  }
+}
