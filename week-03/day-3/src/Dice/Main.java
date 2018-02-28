7111dcf65608d3993909868bcef6ddea3c0f3bac
laszlosebok <hun.slaszlo@gmail.com>
2018-02-28 12:25:14 +0100

    feat(UseClass): Exercise 4

---
+++ week-03/day-3/src/Dice/Main.java
@@ -0,0 +1,27 @@
+package Dice;
+
+public class Main {
+  public static void main(String[] args) {
+    Dice myDice = new Dice();
+    myDice.roll();
+    checkDices(myDice);
+    System.out.println(myDice.toString());
+  }
+  
+  public static void checkDices(Dice myDice) {
+    for (int index = 0; index < myDice.getCurrent().length; index++) {
+      if (myDice.getCurrent(index) != 6) {
+        reRollUntil6(myDice, index);
+      }
+    }
+  }
+  public static void reRollUntil6(Dice myDice, int index) {
+    boolean isSix = false;
+    while (!isSix) {
+      myDice.reroll(index);
+      if (myDice.getCurrent(index) == 6) {
+        isSix = true;
+      }
+    }
+  }
+}
