e1598a02c3cc61349e2798d5e9a12aeaefdc0cbf
laszlosebok <hun.slaszlo@gmail.com>
2018-03-04 12:24:10 +0100

    feat(Tree): adds variable for line length changing

--- week-03/day-5/src/tree/Tree.java
+++ week-03/day-5/src/tree/Tree.java
@@ -6,7 +6,7 @@ import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Tree {
-  private static final int LINE_LENGTH = 80;
+  private static final int LINE_LENGTH_DIFF = 10;
   private static final int ANGLE_DIFF = 20;
   
   private static int WIDTH = 1500;
@@ -16,34 +16,35 @@ public class Tree {
     // https://github.com/greenfox-academy/teaching-materials/blob/master/project/fractals/tree.png
     
     int n = 0;
+    int lineLength = 80;
     int[] startingCoordinates = {WIDTH/2, HEIGHT, WIDTH/2, HEIGHT};
     int angle = 0;
-    drawLines(graphics, startingCoordinates, angle, n);
+    drawLines(graphics, startingCoordinates, lineLength, angle, n);
   }
   
-  private static void drawLines(Graphics graphics, int[] startingCoordinates, int angle, int n) {
+  private static void drawLines(Graphics graphics, int[] startingCoordinates, int lineLength, int angle, int n) {
     
     // Don't change this above 10!
     if (n > 8) {
       return;
     }
     
-    int[] newCoordinates = calcCoordinates(startingCoordinates, angle);
+    int[] newCoordinates = calcCoordinates(startingCoordinates, lineLength, angle);
     Line.draw(graphics, newCoordinates);
     
-    drawLines(graphics, newCoordinates, angle, n+1);
-    drawLines(graphics, newCoordinates, angle + ANGLE_DIFF, n+1);
-    drawLines(graphics, newCoordinates, angle - ANGLE_DIFF, n+1);
+    drawLines(graphics, newCoordinates, lineLength, angle, n+1);
+    drawLines(graphics, newCoordinates, lineLength - LINE_LENGTH_DIFF,angle + ANGLE_DIFF, n+1);
+    drawLines(graphics, newCoordinates, lineLength - LINE_LENGTH_DIFF,angle - ANGLE_DIFF, n+1);
     
   }
   
-  private static int[] calcCoordinates(int[] startingCoordinates, int angle) {
+  private static int[] calcCoordinates(int[] startingCoordinates, int lineLength, int angle) {
     int[] newCoordinates = new int[4];
     
     newCoordinates[0] = startingCoordinates[2];
     newCoordinates[1] = startingCoordinates[3];
-    newCoordinates[2] = ((int) (startingCoordinates[2] + LINE_LENGTH * Math.sin(Math.toRadians(angle))));
-    newCoordinates[3] = ((int) (startingCoordinates[3] - LINE_LENGTH * Math.cos(Math.toRadians(angle))));
+    newCoordinates[2] = ((int) (startingCoordinates[2] + lineLength * Math.sin(Math.toRadians(angle))));
+    newCoordinates[3] = ((int) (startingCoordinates[3] - lineLength * Math.cos(Math.toRadians(angle))));
     
     return newCoordinates;
   }
