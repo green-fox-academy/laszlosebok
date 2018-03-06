f0c46f33fc29198ab212dbf006fa41ea2a264655
laszlosebok <hun.slaszlo@gmail.com>
2018-03-06 12:59:04 +0100

    feat(greenfox): adds mentor class

---
+++ week-04/day-2/src/greenfox/Mentor.java
@@ -0,0 +1,25 @@
+package greenfox;
+
+public class Mentor extends Person {
+  
+  private String level;
+  
+  Mentor(){
+    super();
+    level = "intermediate";
+  }
+  
+  Mentor(String name, int age, String gender, String level) {
+    super(name, age, gender);
+    this.level = level;
+  }
+  
+  public void introduce(){
+    System.out.println("Hi, I'm " + this.getName() + ", a " + this.getAge() + " year old " +
+        this.getGender() + " " + level + " mentor.");
+  }
+  
+  public void getGoal() {
+    System.out.println("Educate brilliant junior software developers.");
+  }
+}
