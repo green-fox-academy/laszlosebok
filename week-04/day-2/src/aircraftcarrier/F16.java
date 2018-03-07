ed1fa9897e34f2688628984522ea32805a7fc818
laszlosebok <hun.slaszlo@gmail.com>
2018-03-07 16:22:25 +0100

    refactor(aircraftcarrier): changed aircraft modifiers

--- week-04/day-2/src/aircraftcarrier/F16.java
+++ week-04/day-2/src/aircraftcarrier/F16.java
@@ -1,10 +1,10 @@
 package aircraftcarrier;
 
-public class F16 extends Aircraft{
+class F16 extends Aircraft{
   
-  public static final String TYPE = "F16";
-  public static final int BASE_DAMAGE = 30;
-  public static final int MAX_AMMO = 8;
+  private static final String TYPE = "F16";
+  private static final int BASE_DAMAGE = 30;
+  private static final int MAX_AMMO = 8;
   
   F16() {
     super(TYPE, MAX_AMMO, BASE_DAMAGE);
--- week-04/day-2/src/aircraftcarrier/F35.java
+++ week-04/day-2/src/aircraftcarrier/F35.java
@@ -1,10 +1,10 @@
 package aircraftcarrier;
 
-public class F35 extends Aircraft {
+class F35 extends Aircraft {
   
-  public static final String TYPE = "F35";
-  public static final int BASE_DAMAGE = 50;
-  public static final int MAX_AMMO = 12;
+  private static final String TYPE = "F35";
+  private static final int BASE_DAMAGE = 50;
+  private static final int MAX_AMMO = 12;
   
   F35() {
     super(TYPE, MAX_AMMO, BASE_DAMAGE);
