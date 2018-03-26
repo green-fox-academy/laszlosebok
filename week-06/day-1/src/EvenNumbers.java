36a571f52358acc18142a6ec3252d43ce77055d5
laszlosebok <hun.slaszlo@gmail.com>
2018-03-26 11:30:18 +0200

    feat(streams): creats stream method that gets even numbers out of a list

---
+++ week-06/day-1/src/EvenNumbers.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EvenNumbers {
+  
+  // Write a Stream Expression to get the even numbers from the following array:
+  
+  
+  
+  public static void main(String[] args) {
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+    
+    List<Integer> evenNumbers = numbers
+        .stream()
+        .filter(n -> n % 2 == 0)
+        .collect(Collectors.toList());
+  
+    System.out.println(evenNumbers);
+  }
+  
+  
+  
+}
