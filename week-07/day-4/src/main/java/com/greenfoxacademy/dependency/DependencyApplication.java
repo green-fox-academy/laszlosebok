14ff44a049082f63146b71b4c0b313b1781c5063
laszlosebok <hun.slaszlo@gmail.com>
2018-04-05 17:23:47 +0200

    feat(dependencies): hello di exercise

---
+++ week-07/day-4/src/main/java/com/greenfoxacademy/dependency/DependencyApplication.java
@@ -0,0 +1,28 @@
+package com.greenfoxacademy.dependency;
+
+import com.greenfoxacademy.dependency.model.MyColor;
+import com.greenfoxacademy.dependency.service.Printer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyApplication implements CommandLineRunner{
+
+  @Autowired
+	private Printer printer;
+  
+  @Autowired
+  private MyColor myColor;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		printer.log("Some string");
+		myColor.printColor();
+	}
+}
