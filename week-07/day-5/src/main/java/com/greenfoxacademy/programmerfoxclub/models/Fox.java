45fa814d011c13539c9e897497f8c5930c0e5dba
laszlosebok <hun.slaszlo@gmail.com>
2018-04-06 10:09:58 +0200

    feat(Fox): creates Fox class skeleton

---
+++ week-07/day-5/src/main/java/com/greenfoxacademy/programmerfoxclub/models/Fox.java
@@ -0,0 +1,45 @@
+package com.greenfoxacademy.programmerfoxclub.models;
+
+public class Fox {
+  
+  private String name;
+  private List<Trick> tricks;
+  private String food;
+  private String drink;
+  
+  public Fox(){
+  }
+  
+  //Getters and Setters
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+  
+  public List<Trick> getTricks() {
+    return tricks;
+  }
+  
+  public void setTricks(List<Trick> tricks) {
+    this.tricks = tricks;
+  }
+  
+  public String getFood() {
+    return food;
+  }
+  
+  public void setFood(String food) {
+    this.food = food;
+  }
+  
+  public String getDrink() {
+    return drink;
+  }
+  
+  public void setDrink(String drink) {
+    this.drink = drink;
+  }
+}
