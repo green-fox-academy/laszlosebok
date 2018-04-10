8affd29210040c4b1ee5ce32945a96a8bb999755
laszlosebok <hun.slaszlo@gmail.com>
2018-04-10 17:15:16 +0200

    feat(Todo): adds createdAt field to Todo class, displays in relavant templates

--- week-08/day-2/mysqltodo/src/main/java/com/greenfoxacademy/mysqltodo/models/Todo.java
+++ week-08/day-2/mysqltodo/src/main/java/com/greenfoxacademy/mysqltodo/models/Todo.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
 public class Todo {
@@ -16,8 +17,10 @@ public class Todo {
   private String title;
   private boolean urgent;
   private boolean done;
+  private LocalDateTime createdAt;
   
   public Todo(){
+    createdAt = LocalDateTime.now();
   }
   
   public Todo(String title){
@@ -71,5 +74,13 @@ public class Todo {
   
   public void setDone(boolean done) {
     this.done = done;
+  }
+  
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+  
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
   }
 }
