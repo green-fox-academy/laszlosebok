917e48f1fa3a09102a8b37b3d1b895c72fd85d9d
laszlosebok <hun.slaszlo@gmail.com>
2018-04-10 11:26:56 +0200

    feat(models): implements Todo class

--- week-08/day-2/h2todo/src/main/java/com/greenfoxacademy/h2todo/models/Todo.java
+++ week-08/day-2/h2todo/src/main/java/com/greenfoxacademy/h2todo/models/Todo.java
@@ -1,4 +1,52 @@
 package com.greenfoxacademy.h2todo.models;
 
 public class Todo {
+
+  // Add a Todo class with the fields(id:Long, title:String, urgent:boolean(default false), done:boolean(default false))
+  
+  private long id;
+  private String title;
+  private boolean urgent;
+  private boolean done;
+  
+  public Todo(){
+  }
+  
+  public Todo(String title){
+    this();
+    this.title = title;
+  }
+  
+  // Getters & Setters
+  public long getId() {
+    return id;
+  }
+  
+  public void setId(long id) {
+    this.id = id;
+  }
+  
+  public String getTitle() {
+    return title;
+  }
+  
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  
+  public boolean isUrgent() {
+    return urgent;
+  }
+  
+  public void setUrgent(boolean urgent) {
+    this.urgent = urgent;
+  }
+  
+  public boolean isDone() {
+    return done;
+  }
+  
+  public void setDone(boolean done) {
+    this.done = done;
+  }
 }
