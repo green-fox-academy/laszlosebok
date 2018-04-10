a2e1b11733bae1300a15b282ab7227b043b79973
laszlosebok <hun.slaszlo@gmail.com>
2018-04-10 11:47:04 +0200

    feat(todo): adds @Entity annotation to model and selects primary key

--- week-08/day-2/h2todo/src/main/java/com/greenfoxacademy/h2todo/models/Todo.java
+++ week-08/day-2/h2todo/src/main/java/com/greenfoxacademy/h2todo/models/Todo.java
@@ -1,9 +1,17 @@
 package com.greenfoxacademy.h2todo.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Todo {
 
   // Add a Todo class with the fields(id:Long, title:String, urgent:boolean(default false), done:boolean(default false))
   
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Id
   private long id;
   private String title;
   private boolean urgent;
@@ -15,6 +23,11 @@ public class Todo {
   public Todo(String title){
     this();
     this.title = title;
+  }
+  
+  public Todo(String title, boolean urgent) {
+    this(title);
+    this.urgent = urgent;
   }
   
   // Getters & Setters
