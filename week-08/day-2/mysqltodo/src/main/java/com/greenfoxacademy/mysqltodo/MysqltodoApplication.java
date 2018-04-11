6d8062eedf5c2e16b52e78690efc02feac3b4e69
laszlosebok <hun.slaszlo@gmail.com>
2018-04-11 16:17:10 +0200

    feat(styling): adds bootstrap css to templates

--- week-08/day-2/mysqltodo/src/main/java/com/greenfoxacademy/mysqltodo/MysqltodoApplication.java
+++ week-08/day-2/mysqltodo/src/main/java/com/greenfoxacademy/mysqltodo/MysqltodoApplication.java
@@ -1,15 +1,12 @@
 package com.greenfoxacademy.mysqltodo;
 
-import com.greenfoxacademy.mysqltodo.models.Todo;
-import com.greenfoxacademy.mysqltodo.respositories.TodoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MysqltodoApplication implements CommandLineRunner{
- 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MysqltodoApplication.class, args);
 	}
