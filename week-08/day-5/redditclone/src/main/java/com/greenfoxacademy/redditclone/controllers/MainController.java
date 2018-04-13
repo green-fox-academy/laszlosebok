09963e97007d3053865f0242a5b9bef030276db2
laszlosebok <hun.slaszlo@gmail.com>
2018-04-13 10:37:02 +0200

    feat(MainController): root end point now returns a template instead of responsebody, autowires MainService to controller

--- week-08/day-5/redditclone/src/main/java/com/greenfoxacademy/redditclone/controllers/MainController.java
+++ week-08/day-5/redditclone/src/main/java/com/greenfoxacademy/redditclone/controllers/MainController.java
@@ -1,15 +1,28 @@
 package com.greenfoxacademy.redditclone.controllers;
 
+import com.greenfoxacademy.redditclone.models.Post;
+import com.greenfoxacademy.redditclone.services.MainService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class MainController {
   
+  private final MainService mainService;
+  
+  @Autowired
+  public MainController(MainService mainService) {
+    this.mainService = mainService;
+  }
+  
   @GetMapping("/")
-  @ResponseBody
-  public String showMainPage() {
-    return "Hello World";
+  public String showMainPage(Model model) {
+    List<Post> posts = mainService.findAllPosts();
+    model.addAttribute("posts", posts);
+    return "index";
   }
 }
