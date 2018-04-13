e84d5758f575ba36c12dc2373d24bc37511a9f67
laszlosebok <hun.slaszlo@gmail.com>
2018-04-13 10:22:05 +0200

    feat(services): creates MainService interface

---
+++ week-08/day-5/redditclone/src/main/java/com/greenfoxacademy/redditclone/services/MainService.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.redditclone.services;
+
+import com.greenfoxacademy.redditclone.models.Post;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface MainService {
+  
+  List<Post> findAllPosts(); 
+}
