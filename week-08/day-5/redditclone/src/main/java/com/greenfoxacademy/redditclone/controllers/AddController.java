2038745a56886b35b21af4f86bfc8c0c7f0f980b
laszlosebok <hun.slaszlo@gmail.com>
2018-04-13 12:04:44 +0200

    feat(AddController): implements showAddPage logic

--- week-08/day-5/redditclone/src/main/java/com/greenfoxacademy/redditclone/controllers/AddController.java
+++ week-08/day-5/redditclone/src/main/java/com/greenfoxacademy/redditclone/controllers/AddController.java
@@ -1,10 +1,19 @@
 package com.greenfoxacademy.redditclone.controllers;
 
+import com.greenfoxacademy.redditclone.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/add")
 public class AddController {
   
+  @GetMapping
+  public String showAddPage(Model model) {
+    Post post = postFactory.create();
+    model.addAttribute("post", post);
+    return "add-form";
+  }
 }
