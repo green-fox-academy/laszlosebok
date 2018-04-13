723fcdd4947ec546c1a3487fe5359de1a71dbc06
laszlosebok <hun.slaszlo@gmail.com>
2018-04-13 12:07:06 +0200

    feat(factories): creates PostFactory class

---
+++ week-08/day-5/redditclone/src/main/java/com/greenfoxacademy/redditclone/factories/PostFactory.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy.redditclone.factories;
+
+import com.greenfoxacademy.redditclone.models.Post;
+
+public class PostFactory {
+  
+  public Post create(){
+    return new Post();
+  }
+}
