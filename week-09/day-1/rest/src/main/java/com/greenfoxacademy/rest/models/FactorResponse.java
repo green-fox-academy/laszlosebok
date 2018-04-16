e032e6473b9c6ab7b20be15503cc1318f097f864
laszlosebok <hun.slaszlo@gmail.com>
2018-04-16 14:17:11 +0200

    feat(models): creates FactorResponse class

---
+++ week-09/day-1/rest/src/main/java/com/greenfoxacademy/rest/models/FactorResponse.java
@@ -0,0 +1,26 @@
+package com.greenfoxacademy.rest.models;
+
+public class FactorResponse {
+  
+  private long result;
+  
+  public FactorResponse(int until) {
+    result = calculateFactor(until);
+  }
+  
+  private long calculateFactor(int until) {
+    long result = 0;
+    for (int i = 1; i < until+1; i++) {
+      result *= i;
+    }
+    return result;
+  }
+  
+  public long getResult() {
+    return result;
+  }
+  
+  public void setResult(long result) {
+    this.result = result;
+  }
+}
