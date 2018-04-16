cab61b3552f0456c9e47a49fd5bfd266b53ca4f0
laszlosebok <hun.slaszlo@gmail.com>
2018-04-16 13:50:35 +0200

    feat(AppendaRestController): implements logic for endpoint

--- week-09/day-1/rest/src/main/java/com/greenfoxacademy/rest/controllers/AppendaRestController.java
+++ week-09/day-1/rest/src/main/java/com/greenfoxacademy/rest/controllers/AppendaRestController.java
@@ -1,16 +1,25 @@
 package com.greenfoxacademy.rest.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.greenfoxacademy.rest.services.AppendaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/appenda/")
 public class AppendaRestController {
   
+  private final AppendaService appendaService;
+  
+  @Autowired
+  public AppendaRestController(AppendaService appendaService) {
+    this.appendaService = appendaService;
+  }
+  
   @GetMapping("{appendable}")
   public Object append(@PathVariable(value = "appendable") String appendee) {
-  return null;
+    return appendaService.createAppendaResponse(appendee);
   }
 }
--- week-09/day-1/rest/src/main/java/com/greenfoxacademy/rest/services/AppendaServiceImpl.java
+++ week-09/day-1/rest/src/main/java/com/greenfoxacademy/rest/services/AppendaServiceImpl.java
@@ -3,7 +3,9 @@ package com.greenfoxacademy.rest.services;
 import com.greenfoxacademy.rest.factories.AppendaResponseFactory;
 import com.greenfoxacademy.rest.models.AppendaResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AppendaServiceImpl implements AppendaService {
   
   private final AppendaResponseFactory appendaResponseFactory;
