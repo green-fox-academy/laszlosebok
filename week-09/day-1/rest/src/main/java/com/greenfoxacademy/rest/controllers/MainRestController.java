d74649b84e73e7ce574f55233f8ccf320b37f0eb
laszlosebok <hun.slaszlo@gmail.com>
2018-04-16 13:03:45 +0200

    feat(controller): creates separate controller for rest related operations

--- week-09/day-1/rest/src/main/java/com/greenfoxacademy/rest/controllers/MainRestController.java
+++ week-09/day-1/rest/src/main/java/com/greenfoxacademy/rest/controllers/MainRestController.java
@@ -1,31 +1,22 @@
 package com.greenfoxacademy.rest.controllers;
 
-import com.greenfoxacademy.rest.models.DoubleResponse;
 import com.greenfoxacademy.rest.services.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class MainController {
+@RestController
+public class MainRestController {
   
   private final MainService mainService;
   
   @Autowired
-  public MainController(MainService mainService) {
+  public MainRestController(MainService mainService) {
     this.mainService = mainService;
   }
   
-  @GetMapping("/")
-  public String showMain() {
-    
-    return "index";
-  }
-  
   @GetMapping("/doubling")
-  @ResponseBody
   public Object doubling(@RequestParam("input") String input) {
     Integer number = mainService.parseInt(input);
     if (number != null) {
---
+++ week-09/day-1/rest/src/main/java/com/greenfoxacademy/rest/controllers/MainWebController.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy.rest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainWebController {
+  
+  
+  @GetMapping("/")
+  public String showMain() {
+    
+    return "index";
+  }
+}
