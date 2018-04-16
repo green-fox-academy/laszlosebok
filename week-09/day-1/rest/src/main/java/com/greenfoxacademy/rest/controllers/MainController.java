8c789578ef550ba059d0bd88211474fb0d14dc14
laszlosebok <hun.slaszlo@gmail.com>
2018-04-16 12:21:49 +0200

    feat(MainController): creates endpoint '/doubling'

--- week-09/day-1/rest/src/main/java/com/greenfoxacademy/rest/controllers/MainController.java
+++ week-09/day-1/rest/src/main/java/com/greenfoxacademy/rest/controllers/MainController.java
@@ -2,6 +2,8 @@ package com.greenfoxacademy.rest.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -10,5 +12,11 @@ public class MainController {
   public String showMain() {
     
     return "index";
+  }
+  
+  @GetMapping("/doubling")
+  @ResponseBody
+  public String doubling(@RequestParam("input") String input) {
+    return null;
   }
 }
